53149b5e1d309f4e02ed5e2d250e61f9351e29e8
MrH4554N <vohoangtan2004@gmail.com>
2024-12-14 22:22:12 +0700

    Update policy.py
    Update manual

--- MM_Assignment/policy.py
+++ MM_Assignment/policy.py
@@ -1,3 +1,27 @@
+#HƯỚNG DẪN SỬ DỤNG NẾU KHÔNG THỂ CHẠY PROJECT!
+#
+#DOWNLOAD PROJECT TẠI: https://github.com/MrH4554N/MM_Assignment
+#
+#Yêu cầu:
+#  Python 3.11 trở lên
+#  Các thư viện yêu cầu
+#    numpy
+#    matplotlib
+#
+# Cài đặt thư viện
+#  Mở terminal và chạy lệnh cài đặt thư viện: pip install numpy matplotlib
+#
+# Cách sử dụng
+#   Tải 2 file main.py và policy.py, lưu chung 1 folder
+#   Cài đặt các thư viện cần thiết
+#   Chạy chương trình bằng cách gõ lệnh trên terminal: python main.py
+#
+# Các giá trị cần nhập:
+#   Chiều dài, chiều rộng miếng vật liệu vật ban đầu (Rectangle Width, Rectangle Height)
+#   Kích thước các mảnh cần cắt và số lượng (Slice Width, Slice Height, Demand)
+#   Sau khi nhập các giá trị cần thiết, ấn Start Program để bắt đầu chương trình
+
+
 import random
 import numpy as np
 import matplotlib.pyplot as plt
